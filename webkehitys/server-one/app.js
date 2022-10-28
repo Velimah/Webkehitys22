dff88b2f9338d7fb9958017c394edd1ba3317086
veli-matti heino <veli-matti.heino@metropolia.fi>
2022-10-28 14:30:35 +0300

    week1 new branch

--- webkehitys/server-one/app.js
+++ webkehitys/server-one/app.js
@@ -4,13 +4,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.get('/catinfo', (req, res) => {
   const cat = {
     name: 'Paul',
     birthdate: '2010-12-25',
-    weight: 5,
+    weight: 15,
   };
   res.json(cat);
 });
