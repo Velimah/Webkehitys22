463453806acce1f6df558fe56f2fb8c626732990
veli-matti heino <veli-matti.heino@metropolia.fi>
2022-11-01 11:34:14 +0200

    week2 new branch

--- webkehitys/week2/controllers/catController.js
+++ webkehitys/week2/controllers/catController.js
@@ -1,2 +1,14 @@
 'use strict';
-// catController
+const {cats} = require('../models/catModel');
+
+const cat_list_get = (req, res) => {
+  res.json(cats);
+};
+
+const cat_get = (req, res) => {
+  res.json(cats[req.params.id-1]);
+};
+
+module.exports = {
+  cat_list_get, cat_get,
+};
--- webkehitys/week2/routes/catRoute.js
+++ webkehitys/week2/routes/catRoute.js
@@ -2,14 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
+const {cat_list_get, cat_get} = require('../controllers/catController');
 
-router.get('/', (req, res) => {
-  res.send('From this endpoint you can get cats.')
-});
+router.get('/', cat_list_get);
 
-router.get('/:id/', (req, res) => {
-  res.send(`You requested a cat whose id is ${req.params.id}`);
-});
+router.get('/:id/', cat_get);
 
 router.post('/', (req, res) => {
   res.send('With this endpoint you can add cats.')
