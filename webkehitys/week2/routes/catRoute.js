7defcbf03459cf4a0121ba2d9a8090d360af0160
veli-matti heino <veli-matti.heino@metropolia.fi>
2022-11-04 11:50:30 +0200

    week2 database

--- webkehitys/week2/routes/catRoute.js
+++ webkehitys/week2/routes/catRoute.js
@@ -6,7 +6,7 @@ const multer = require('multer');
 const upload = multer({dest: './uploads/'});
 
 const router = express.Router();
-const {cat_list_get, cat_get, cat_post} = require(
+const {cat_list_get, cat_get, cat_post, cat_update_put, cat_delete} = require(
     '../controllers/catController');
 
 router.get('/', cat_list_get);
@@ -15,12 +15,8 @@ router.get('/:id/', cat_get);
 
 router.post('/', upload.single('cat'), cat_post);
 
-router.put('/', (req, res) => {
-  res.send('With this endpoint you can edit cats.');
-});
+router.put('/', cat_update_put);
 
-router.delete('/', (req, res) => {
-  res.send('With this endpoint you can delete cats.');
-});
+router.delete('/:id/', cat_delete);
 
 module.exports = router;
