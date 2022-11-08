0f53ebb8c4a73ba0566882b5e087e3f400e03bd2
veli-matti heino <veli-matti.heino@metropolia.fi>
2022-11-08 12:15:39 +0200

    week2 database error handling

--- webkehitys/week2/controllers/catController.js
+++ webkehitys/week2/controllers/catController.js
@@ -2,6 +2,7 @@
 
 const {getCat, getAllCats, addCat, updateCat, deleteCat} = require(
     '../models/catModel');
+const {httpError} = require('../utils/errors');
 
 const cat_list_get = async (req, res, next) => {
   const kissat = await getAllCats(next);
@@ -17,7 +18,7 @@ const cat_get = async (req, res, next) => {
   }
 };
 
-const cat_post = async (req, res) => {
+const cat_post = async (req, res, next) => {
   console.log('cat_post', req.body, req.file);
   const data = [
     req.body.name,
@@ -27,7 +28,7 @@ const cat_post = async (req, res) => {
     req.file.filename,
   ];
 
-  const result = await addCat(data);
+  const result = await addCat(data, next);
   if (result.affectedRows > 0) {
     res.json({
       message: 'cat added',
@@ -38,28 +39,33 @@ const cat_post = async (req, res) => {
   }
 };
 
-const cat_update_put = async (req, res) => {
-  console.log('cat_put', req.body);
-  const data = [
-    req.body.name,
-    req.body.birthdate,
-    req.body.weight,
-    req.body.owner,
-    req.body.id,
-  ];
+const cat_update_put = async (req, res, next) => {
+  try {
+    console.log('cat_put', req.body);
+    const data = [
+      req.body.name,
+      req.body.birthdate,
+      req.body.weight,
+      req.body.owner,
+      req.body.id,
+    ];
 
-  const result = await updateCat(data);
-  if(result.affectedRows > 0) {
-    res.json({
-      message: 'cat modified',
-    });
-  } else {
-    res.send('virhe')
+    const result = await updateCat(data, next);
+    if (result.affectedRows > 0) {
+      res.json({
+        message: 'cat modified',
+      });
+    } else {
+      next(httpError('No cat modified', 400));
+    }
+  } catch (e) {
+    console.error('cat_put', e.message);
+    next(httpError('Invalid input', 400));
   }
-}
+};
 
-const cat_delete = async (req, res) => {
-  const result = await deleteCat(req.params.id);
+const cat_delete = async (req, res, next) => {
+  const result = await deleteCat(req.params.id, next);
   if (result.affectedRows > 0) {
     res.json({
       message: 'cat deleted',
--- webkehitys/week2/controllers/userController.js
+++ webkehitys/week2/controllers/userController.js
@@ -2,13 +2,13 @@
 
 const {getAllUsers, getUser, addUser} = require('../models/userModel');
 
-const users_get = async (req, res) => {
-  const users = await getAllUsers();
+const users_get = async (req, res, next) => {
+  const users = await getAllUsers(next);
   res.json(users);
 };
 
-const user_get = async (req, res) => {
-  const user = await getUser(req.params.id);
+const user_get = async (req, res, next) => {
+  const user = await getUser(req.params.id, next);
   if (user.length > 0) {
     res.json(user.pop());
   } else {
@@ -16,15 +16,15 @@ const user_get = async (req, res) => {
   }
 };
 
-const user_create_post = async (req, res) => {
-  console.log('user_post', req.body);
+const user_create_post = async (req, res, next) => {
+  console.log('user_post', req.body, next);
   const data = [
     req.body.name,
     req.body.email,
     req.body.passwd,
   ];
 
-  const result = await addUser(data);
+  const result = await addUser(data, next);
   if (result.affectedRows > 0) {
     res.json({
       message: 'user added',
